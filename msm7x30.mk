a46b0e07d8e97256684b2c81794c90f8fbe00944
Michael Bestas <mikeioannina@gmail.com>
2014-11-19 03:46:21 +0200

    msm7x30: Include google's media codecs
    * Deduplicate the codecs
    
    Change-Id: I2c454b2ce680f172ec2ae4417ea186fa6eb4fff6
    
    Conflicts:
    
    	msm7x30.mk
    	rootdir/system/etc/media_codecs.xml
    
    * Adapted for OpenDesireProject repo by Mustaavalkosta

--- msm7x30.mk
+++ msm7x30.mk
@@ -46,8 +46,11 @@ PRODUCT_COPY_FILES += \
 
 # media configs
 PRODUCT_COPY_FILES += \
-	$(COMMON_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	$(COMMON_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+	$(COMMON_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
 	$(COMMON_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf
 
 # Camera
