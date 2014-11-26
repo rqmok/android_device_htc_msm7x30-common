72618a9fc6958eb91414bbee550a403b6d6fa79f
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-11-25 17:36:24 -0800

    msm7x30: fix default USB configuration
    Apparently adding adb is not necessary. It came out as mtp,adb,adb in default.prop with this.
    
    Change-Id: I7d23b4c08c66d088147db387923e08ba06943e91

--- msm7x30.mk
+++ msm7x30.mk
@@ -149,7 +149,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # set default USB configuration
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp,adb
+	persist.sys.usb.config=mtp
 
 # Extra debugging props
 PRODUCT_PROPERTY_OVERRIDES += \
