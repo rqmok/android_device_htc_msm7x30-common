dedeff1e056b8d8143534d7d29104b7c8fab8de3
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-12-06 00:08:00 +0200

    libril: clean up makefile
    Change-Id: I62bf4ebe4ef22ecc1153f9c3e780a058e30f5e6f

--- libril/Android.mk
+++ libril/Android.mk
@@ -18,35 +18,11 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy \
     librilutils
 
-#LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
-
-ifeq ($(SIM_COUNT), 2)
-    LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
-endif
+LOCAL_CFLAGS :=
 
 LOCAL_MODULE:= libril
 
 include $(BUILD_SHARED_LIBRARY)
 
-
-# For RdoServD which needs a static library
-# =========================================
-ifneq ($(ANDROID_BIONIC_TRANSITION),)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-    ril.cpp
-
-LOCAL_STATIC_LIBRARIES := \
-    libutils_static \
-    libcutils \
-    librilutils_static
-
-LOCAL_CFLAGS :=
-
-LOCAL_MODULE:= libril_static
-
-include $(BUILD_STATIC_LIBRARY)
-endif # ANDROID_BIONIC_TRANSITION
 endif # TARGET_BOARD_PLATFORM
 endif # BOARD_PROVIDES_LIBRIL
