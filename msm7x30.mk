df15149bc07707a195f64e67f61d7770091ddafc
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-01-05 01:37:18 +0200

    msm7x30: add wpa_supplicant_overlay.conf
    * disable scan offload
    
    Change-Id: If210cea869020a1e4e90fa286ac012d7e7667c39

--- msm7x30.mk
+++ msm7x30.mk
@@ -93,6 +93,9 @@ PRODUCT_PACKAGES += \
 	wpa_supplicant \
 	wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+	$(COMMON_PATH)/rootdir/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
 # HAL
 PRODUCT_PACKAGES += \
 	power.msm7x30 \
