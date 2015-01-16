7ec8619a8e4cadd6999fb54724667856178a2f71
Michael Bestas <mikeioannina@gmail.com>
2015-01-16 19:15:09 +0200

    msm7x30-common: Enable high-end gfx by default
    * Lollipop UI is ugly on low-ram devices
    * Disable low-end gfx until the UI is proper
    
    Change-Id: I7e565420cf623908398e0448cef5845b5975ad72

--- msm7x30.mk
+++ msm7x30.mk
@@ -127,7 +127,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # For applications to determine if they should turn off specific memory-intensive
 # features that work poorly on low-memory devices.
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=true
+	ro.config.low_ram=true \
+	persist.sys.force_highendgfx=true
 
 # ODPUpdater
 PRODUCT_PACKAGES += ODPUpdater
