ce7e93c42fa9b5f7d7198232302adff43722b038
jonas2515 <jonas2515@users.noreply.github.com>
2015-03-25 21:58:24 +0100

    BoardConfig: Clean up and update for lollipop

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
 
 BOARD_VENDOR := htc
@@ -7,60 +21,49 @@ TARGET_NO_RADIOIMAGE := true
 
 # General
 TARGET_BOARD_PLATFORM := msm7x30
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := scorpion
+TARGET_CPU_SMP := false
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 BOARD_USES_QCOM_HARDWARE := true
 
-# Build EXT4 and F2FS tools
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 
+# Build EXT4 and F2FS tools
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Audio
 BOARD_HAVE_HTC_AUDIO := true
 #BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
 #BOARD_HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB := true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
-#COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
-#BOARD_HAVE_QCOM_FM := true
-#BOARD_USES_QCOM_AUDIO_LPA := true
-#BOARD_USES_QCOM_AUDIO_RESETALL := true
-#BOARD_USES_QCOM_AUDIO_SPEECH := true
-#BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
+AUDIO_FEATURE_ENABLED_INCALL_MUSIC := false
+AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := false
+AUDIO_FEATURE_ENABLED_PROXY_DEVICE := false
 
 # Display
-BOARD_USES_LEGACY_QCOM_DISPLAY := true
 BOARD_EGL_CFG := device/htc/msm7x30-common/rootdir/system/lib/egl/egl.cfg
-USE_OPENGL_RENDERER := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_NO_ADAPTIVE_PLAYBACK := true
 TARGET_NO_HW_VSYNC := true
-TARGET_QCOM_LEGACY_OMX := true
 TARGET_USES_ION := true
+USE_OPENGL_RENDERER := true
+HWUI_COMPILE_FOR_PERF := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
 
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
 
 # Media
-#TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-CAMERA_USES_SURFACEFLINGER_CLIENT_STUB := true
-TARGET_DISABLE_ARM_PIE := true
 BOARD_NEEDS_MEMORYHEAPPMEM := true
 BOARD_USES_PMEM_ADSP := true
 USE_CAMERA_STUB := false
-#COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DNEEDS_VECTORIMPL_SYMBOLS -DQCOM_LEGACY_CAM_PARAMS
 COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DQCOM_LEGACY_CAM_PARAMS
 TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
@@ -94,14 +97,32 @@ TARGET_BOOTANIMATION_USE_RGB565 := true
 # Misc
 TARGET_PROVIDES_LIBLIGHT := true
 
+BLOCK_BASED_OTA := false
+
 BOARD_USE_NEW_LIBRIL_HTC := true
 BOARD_USES_LEGACY_RIL := true
 
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
+TARGET_NEEDS_NON_PIE_SUPPORT := true
+
+EXTENDED_FONT_FOOTPRINT := true
+
+
+# Use dlmalloc instead of jemalloc because it's
+# supposedly better in single-threaded applications
+# http://blog.poweredbytoast.com/memory-allocators
+#MALLOC_IMPL := dlmalloc
 
 # rmt_storage
 BOARD_USES_LEGACY_MMAP := true
 
-# Device Settings
-#BOARD_DEVICE_SETTINGS := device/htc/msm7x30-common/DeviceSettings
+USE_DEXPREOPT := true
+
+# Dexpreopt
+ifeq ($(USE_DEXPREOPT),true)
+    # Enable dex-preoptimization to speed up first boot sequence
+    ifeq ($(HOST_OS),linux)
+        ifeq ($(WITH_DEXPREOPT),)
+            WITH_DEXPREOPT := true
+        endif
+    endif
+endif
