b7f05d90d4654a2a8cf86e1dbb3ba0c58ec36299
jonas2515 <jonas2515@users.noreply.github.com>
2015-03-29 21:22:26 +0200

    msm7x30: Remove egl.cfg

--- msm7x30.mk
+++ msm7x30.mk
@@ -59,11 +59,6 @@ PRODUCT_COPY_FILES += \
 	$(COMMON_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
 	$(COMMON_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf
 
-# Egl.cfg
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/lib/egl/egl.cfg:system/lib/egl/egl.cfg
-
-
 # Audio
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
