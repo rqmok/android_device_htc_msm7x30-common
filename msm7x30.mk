3c76a7cb92b4dfc9d83021878408b64528b07303
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-05-14 20:10:13 +0300

    msm7x30: use 1024 xhdpi VM heap values
    * These seem to help with reboot issue providing more stable
      reboot times. Test run with wider audience.
    
    Change-Id: If112809a2b8f14e249dd58fdb92c843d6e292f08

--- msm7x30.mk
+++ msm7x30.mk
@@ -130,6 +130,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=true \
 	persist.sys.force_highendgfx=true
 
+# Set our own VM heap values
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.heapstartsize=8m \
+	dalvik.vm.heapgrowthlimit=96m \
+	dalvik.vm.heapsize=256m \
+	dalvik.vm.heaptargetutilization=0.75 \
+	dalvik.vm.heapminfree=2m \
+	dalvik.vm.heapmaxfree=8m
+
 # ODPUpdater
 PRODUCT_PACKAGES += ODPUpdater
 
