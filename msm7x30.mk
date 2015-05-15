fd263c7bc27397329d6773c7d4f6b1ebb2403575
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-05-15 23:50:13 +0300

    msm7x30: remove dalvik heap preset
    Change-Id: Ie36b6f588f6db27775e18697ac6640b38fb45e31

--- msm7x30.mk
+++ msm7x30.mk
@@ -17,8 +17,6 @@ PRODUCT_PACKAGE_OVERLAYS += device/htc/msm7x30-common/overlay
 
 COMMON_PATH := device/htc/msm7x30-common
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 # System Properties
@@ -131,6 +129,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.force_highendgfx=true
 
 # Set our own VM heap values
+# See frameworks/native/build/ for presets
 PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.heapstartsize=8m \
 	dalvik.vm.heapgrowthlimit=96m \
