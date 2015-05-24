0c528e9264a484aca4c6e8a787f7a639b792d106
jonas2515 <jonas2515@users.noreply.github.com>
2015-05-24 22:00:12 +0200

    msm7x30: Remove unneeded props

--- system_prop.mk
+++ system_prop.mk
@@ -49,10 +49,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	debug.egl.hw=1 \
 	debug.egl.recordable.rgba8888=1
 
-# Audio
-PRODUCT_PROPERTY_OVERRIDES += \
-	audio.offload.disable=1
-
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 	camera2.portability.force_api=1
@@ -60,8 +56,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # UMS
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	ro.sys.usb.default.config=mtp \
-	persist.sys.usb.config=mtp \
-	ro.vold.umsdirtyratio=20
+	persist.sys.usb.config=mtp
 
 # Memory
 PRODUCT_PROPERTY_OVERRIDES += \
