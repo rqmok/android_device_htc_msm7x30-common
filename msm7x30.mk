c7f4efb32156a05100eb335d55713092b7492a30
Michael Bestas <mikeioannina@gmail.com>
2015-05-31 17:46:31 +0300

    msm7x30-common: Set dalvik.vm.dex2oat-filter=balanced
    * ~11x runtime performance improvement
    
    Change-Id: I108f10518637334fea65351c54f3d3ad2a0de5e2

--- msm7x30.mk
+++ msm7x30.mk
@@ -99,7 +99,6 @@ PRODUCT_PACKAGES += \
 	wpa_supplicant \
 	wpa_supplicant.conf
 
-
 # GPS
 PRODUCT_PACKAGES += librpc
 
@@ -121,7 +120,7 @@ PRODUCT_PACKAGES += DeviceSettings
 # Use ART small mode and disable watchdog
 # http://source.android.com/devices/tech/dalvik/configure.html#with_art_small_mode
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.dex2oat-filter=interpret-only \
+	dalvik.vm.dex2oat-filter=balanced \
 	dalvik.vm.dex2oat-flags=--no-watch-dog \
 	dalvik.vm.image-dex2oat-filter=speed
 
