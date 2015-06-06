21461ee1dabe11a859847c2f0bec77d1fb0c480b
Michael Bestas <mikeioannina@gmail.com>
2015-06-06 18:29:29 +0300

    msm7x30-common: Don't build recovery_from_boot.p
    Change-Id: I3fcb61b381ffbdd6592d33ab02d44e12db61e01c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,7 @@ BLOCK_BASED_OTA := false
 TARGET_NEEDS_NON_PIE_SUPPORT := true
 TARGET_DISABLE_ARM_PIE := true
 BOARD_PROVIDES_LIBRIL := true
+BOARD_CANT_BUILD_RECOVERY_FROM_BOOT_PATCH := true
 
 # Use dlmalloc instead of jemalloc because it's
 # supposedly better in single-threaded applications
