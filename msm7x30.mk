f6027a7373ad105d1f8cf269259e102cad2ac25a
kylon <kylonux@gmail.com>
2015-06-06 09:58:47 +0200

    msm7x30-common: Set dalvik.vm.dex2oat-filter=balanced

--- msm7x30.mk
+++ msm7x30.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += DeviceSettings
 # Use ART small mode
 # http://source.android.com/devices/tech/dalvik/configure.html#with_art_small_mode
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.dex2oat-filter=interpret-only \
+	dalvik.vm.dex2oat-filter=balanced \
 	dalvik.vm.dex2oat-flags=--no-watch-dog \
 	dalvik.vm.image-dex2oat-filter=speed
 
