896745ce94ef77c00aa16a6db0efb03e54b0bdf3
jrior001 <jriordan001@gmail.com>
2015-09-06 23:20:15 -0400

    move storage overlay to devices
    Change-Id: Id3a0707631fd2dffdec8fcc952b702d366b87dae

--- msm7x30.mk
+++ msm7x30.mk
@@ -13,11 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ifneq ($(filter aceopt,$(TARGET_DEVICE)),)
-PRODUCT_PACKAGE_OVERLAYS += device/htc/msm7x30-common/overlay_variant
-else
+
 PRODUCT_PACKAGE_OVERLAYS += device/htc/msm7x30-common/overlay
-endif
 
 COMMON_PATH := device/htc/msm7x30-common
 
