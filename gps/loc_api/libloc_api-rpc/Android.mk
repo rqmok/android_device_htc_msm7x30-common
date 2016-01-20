bc70b75f633895f37bb0163c7d9784e1257e139a
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-01-20 20:04:25 +0200

    gps: Cleanup makefiles and remove unused code
    Change-Id: I545b791f3804db6a077c2d2fde3c417839c29857

--- gps/loc_api/libloc_api-rpc/Android.mk
+++ gps/loc_api/libloc_api-rpc/Android.mk
@@ -1,18 +1,30 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION),50001)
 $(shell mkdir -p $(OUT)/obj/STATIC_LIBRARIES/libcommondefs-rpc_intermediates/)
 $(shell touch $(OUT)/obj/STATIC_LIBRARIES/libcommondefs-rpc_intermediates/export_includes)
 
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
-AMSS_VERSION:=$(BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION)
-RPC_INC:=inc-$(AMSS_VERSION)
+AMSS_VERSION := $(BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION)
+RPC_INC := inc-$(AMSS_VERSION)
 
-generated_files:= \
-	gen-$(AMSS_VERSION)/loc_api_clnt.c	\
-	gen-$(AMSS_VERSION)/loc_api_cb_xdr.c	\
+generated_files := \
+	gen-$(AMSS_VERSION)/loc_api_clnt.c \
+	gen-$(AMSS_VERSION)/loc_api_cb_xdr.c \
 	gen-$(AMSS_VERSION)/loc_api_common_xdr.c \
 	gen-$(AMSS_VERSION)/loc_api_cb_svc.c \
 	gen-$(AMSS_VERSION)/loc_api_xdr.c \
@@ -21,25 +33,19 @@ generated_files:= \
 	src/loc_apicb_appinit.c \
 	src/loc_api_sync_call.c
 
-LOCAL_SRC_FILES:= $(generated_files)
+LOCAL_SRC_FILES := $(generated_files)
 
-# removed from library build since the client should implement this code.
-#	src/loc_api_cb_server.c
+LOCAL_CFLAGS := -fno-short-enums
+LOCAL_CFLAGS += -include $(RPC_INC)/loc_api_common.h
+LOCAL_CFLAGS += -DDEBUG
+# LOCAL_CFLAGS += -DVERBOSE
+LOCAL_CFLAGS += -DADD_XDR_FLOAT -DADD_XDR_BOOL
 
-LOCAL_CFLAGS:=-fno-short-enums
-LOCAL_CFLAGS+=-include $(RPC_INC)/loc_api_common.h
-LOCAL_CFLAGS+=-DDEBUG
-# LOCAL_CFLAGS+=-DDEBUG -DVERBOSE
+LOCAL_SHARED_LIBRARIES := librpc
+LOCAL_STATIC_LIBRARIES := libcommondefs-rpc
 
-LOCAL_CFLAGS+=-DADD_XDR_FLOAT -DADD_XDR_BOOL
-
-LOCAL_SHARED_LIBRARIES:= librpc
-LOCAL_STATIC_LIBRARIES:= libcommondefs-rpc
-
-
-
-LOCAL_COPY_HEADERS_TO:= libloc_api-rpc/inc
-LOCAL_COPY_HEADERS:= \
+LOCAL_COPY_HEADERS_TO := libloc_api-rpc/inc
+LOCAL_COPY_HEADERS := \
 	$(RPC_INC)/loc_api_cb.h \
 	$(RPC_INC)/loc_api_common.h \
 	$(RPC_INC)/loc_api.h \
@@ -49,16 +55,13 @@ LOCAL_COPY_HEADERS:= \
 	inc/loc_api_rpc_glue.h \
 	inc/loc_api_sync_call.h
 
-LOCAL_C_INCLUDES:= \
+LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH) \
 	$(LOCAL_PATH)/inc \
 	$(LOCAL_PATH)/$(RPC_INC) \
 	$(TARGET_OUT_HEADERS)/libcommondefs-rpc \
 	$(TARGET_OUT_HEADERS)/librpc
 
-LOCAL_MODULE:= libloc_api-rpc
+LOCAL_MODULE := libloc_api-rpc
 
 include $(BUILD_STATIC_LIBRARY)
-
-endif
-endif
--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,7 +1,16 @@
-ifneq ($(TARGET_PROVIDES_GPS_LOC_API),true)
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
 
@@ -19,19 +28,19 @@ LOCAL_SRC_FILES += \
     linked_list.c
 
 LOCAL_CFLAGS += \
-     -fno-short-enums \
-     -D_ANDROID_
+    -fno-short-enums \
+    -D_ANDROID_
 
 ## Includes
 LOCAL_C_INCLUDES:=
 
-LOCAL_COPY_HEADERS_TO:= gps.utils/
-LOCAL_COPY_HEADERS:= \
-   loc_log.h \
-   loc_cfg.h \
-   log_util.h \
-   linked_list.h \
-   msg_q.h
+LOCAL_COPY_HEADERS_TO := gps.utils/
+LOCAL_COPY_HEADERS := \
+    loc_log.h \
+    loc_cfg.h \
+    log_util.h \
+    linked_list.h \
+    msg_q.h
 
 LOCAL_MODULE := libgps.utils
 
@@ -40,7 +49,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
-endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
-endif#TARGET_PROVIDES_GPS_LOC_API
