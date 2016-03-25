90310164b7e42f700383fd696965b0a668b92920
jonas2515 <jonas2515@users.noreply.github.com>
2016-03-25 22:43:42 +0100

    msm7x30: Update power HAL to support profiles
    * Mostly based on the HAL from CyanogenMod/android_device_asus_flo

---
+++ power/power.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+enum {
+    PROFILE_POWER_SAVE = 0,
+    PROFILE_BALANCED,
+    PROFILE_HIGH_PERFORMANCE,
+    PROFILE_BIAS_POWER,
+    PROFILE_BIAS_PERFORMANCE,
+    PROFILE_MAX
+};
+
+typedef struct governor_settings {
+    int is_interactive;
+    int boost;
+    int boostpulse_duration;
+    int go_hispeed_load;
+    int go_hispeed_load_off;
+    int hispeed_freq;
+    int hispeed_freq_off;
+    int timer_rate;
+    int timer_rate_off;
+    int above_hispeed_delay;
+    int io_is_busy;
+    int min_sample_time;
+    char *target_loads;
+    char *target_loads_off;
+    int min_freq;
+    int max_freq;
+} power_profile;
+
+static power_profile profiles[PROFILE_MAX] = {
+    [PROFILE_POWER_SAVE] = {
+        .boost = 0,
+        .boostpulse_duration = 0,
+        .go_hispeed_load = 90,
+        .go_hispeed_load_off = 110,
+        .hispeed_freq = 1024000,
+        .hispeed_freq_off = 1024000,
+        .timer_rate = 20000,
+        .timer_rate_off = 50000,
+        .above_hispeed_delay = 79000,
+        .io_is_busy = 0,
+        .min_sample_time = 60000,
+        .target_loads = "90",
+        .target_loads_off = "95",
+        .min_freq = 245760,
+        .max_freq = 1024000,
+    },
+    [PROFILE_BALANCED] = {
+        .boost = 0,
+        .boostpulse_duration = 60000,
+        .go_hispeed_load = 90,
+        .go_hispeed_load_off = 110,
+        .hispeed_freq = 1200000,
+        .hispeed_freq_off = 1200000,
+        .timer_rate = 20000,
+        .timer_rate_off = 50000,
+        .above_hispeed_delay = 79000,
+        .io_is_busy = 1,
+        .min_sample_time = 60000,
+        .target_loads = "80",
+        .target_loads_off = "90",
+        .min_freq = 245760,
+        .max_freq = 1200000,
+    },
+    [PROFILE_HIGH_PERFORMANCE] = {
+        .boost = 1,
+        .boostpulse_duration = 0, /* prevent unnecessary write */
+        .go_hispeed_load = 50,
+        .go_hispeed_load_off = 110,
+        .hispeed_freq = 1401600,
+        .hispeed_freq_off = 1401600,
+        .timer_rate = 20000,
+        .timer_rate_off = 50000,
+        .above_hispeed_delay = 79000,
+        .io_is_busy = 1,
+        .min_sample_time = 60000,
+        .target_loads = "70",
+        .target_loads_off = "80",
+        .min_freq = 368640,
+        .max_freq = 1401600,
+    },
+    [PROFILE_BIAS_POWER] = {
+        .boost = 0,
+        .boostpulse_duration = 40000,
+        .go_hispeed_load = 90,
+        .go_hispeed_load_off = 110,
+        .hispeed_freq = 1200000,
+        .hispeed_freq_off = 1200000,
+        .timer_rate = 20000,
+        .timer_rate_off = 50000,
+        .above_hispeed_delay = 79000,
+        .io_is_busy = 0,
+        .min_sample_time = 60000,
+        .target_loads = "90",
+        .target_loads_off = "95",
+        .min_freq = 245760,
+        .max_freq = 1200000,
+    },
+    [PROFILE_BIAS_PERFORMANCE] = {
+        .boost = 0,
+        .boostpulse_duration = 60000,
+        .go_hispeed_load = 50,
+        .go_hispeed_load_off = 110,
+        .hispeed_freq = 1401600,
+        .hispeed_freq_off = 1401600,
+        .timer_rate = 20000,
+        .timer_rate_off = 50000,
+        .above_hispeed_delay = 79000,
+        .io_is_busy = 1,
+        .min_sample_time = 60000,
+        .target_loads = "70",
+        .target_loads_off = "80",
+        .min_freq = 368640,
+        .max_freq = 1401600,
+    },
+};
