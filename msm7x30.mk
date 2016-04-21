a3760b761196ecb266ed5fa3c96824d9b8e0e94c
Zeeshan Khan <rqmok@hotmail.com>
2016-04-21 05:32:51 +0000

    msm7x30: Do not use BLE.
    Change-Id: I06436fd1ae08837f12e8bfb6b316afcbbc18ec20

--- msm7x30.mk
+++ msm7x30.mk
@@ -46,7 +46,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
 
 
 # Media config files
