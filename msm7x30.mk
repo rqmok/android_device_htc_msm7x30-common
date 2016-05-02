398e066d36702116ff7cf961b701e092d175bdef
Zeeshan Khan <rqmok1996@gmail.com>
2016-05-02 10:44:37 +0000

    msm7x30: Add BLE permission. * Just in case we need it.
    Change-Id: I24221959f11d997fcfb7bbbc2efd4685b26ff15f

--- msm7x30.mk
+++ msm7x30.mk
@@ -46,8 +46,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
-
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Media config files
 PRODUCT_COPY_FILES += \
